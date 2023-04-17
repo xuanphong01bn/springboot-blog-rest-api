f29901ede5a4250b034dd0d78f7566efcf40b1d0
xuanphong01bn <donhatphong94@gmail.com>
2023-04-17 21:16:34 +0700

    post entity

---
+++ src/main/java/com/springboot/blog/springbootblogrestapi/entity/Post.java
@@ -0,0 +1,30 @@
+package com.springboot.blog.springbootblogrestapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(
+        name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})} // ten khong duoc giong nhau
+)
+public class Post {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+
+    @Column(name="title", nullable = false)
+    private String title;
+    @Column(name="description", nullable = false)
+    private String description;
+    @Column(name="content", nullable = false)
+    private String content;
+
+}
